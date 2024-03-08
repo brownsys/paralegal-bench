f35e66ec9456050be3a3f30198524efe5104955b
Justus Adam <justbldr@amazon.com>
2024-03-08 10:56:07 -0500

    Allow extra arguments

--- hyperswitch/my-policy/src/main.rs
+++ hyperswitch/my-policy/src/main.rs
@@ -121,16 +121,26 @@ struct Args {
     policy: Option<Vec<Policy>>,
     #[clap(long, default_value = "..")]
     source_dir: std::path::PathBuf,
+    #[clap(last = true)]
+    extra_flow_args: Vec<String>,
 }
 
 fn main() -> Result<()> {
     let mut cmd = paralegal_policy::SPDGGenCommand::global();
     cmd.abort_after_analysis();
     cmd.external_annotations("external-annotations.toml");
-    cmd.get_command()
-        .args(["--target", "router", "--", "--lib"]);
     use clap::{Parser, ValueEnum};
     let args: &'static _ = Box::leak(Box::new(Args::parse()));
+    cmd.get_command()
+        .args(["--target", "router"])
+        .args(&args.extra_flow_args);
+    let dashes = args.extra_flow_args.iter().filter(|s| *s == "--").count();
+    if dashes == 0 {
+        cmd.get_command().arg("--");
+    } else {
+        assert_eq!(dashes, 1, "too many '--' in extra args");
+    }
+    cmd.get_command().arg("--lib");
     cmd.run(&args.source_dir)?.with_context(|ctx| {
         for p in args
             .policy
