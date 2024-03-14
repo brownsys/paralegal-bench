54aa731a97d43159648ea15c5dae5b00e14a7f82
Justus Adam <justbldr@amazon.com>
2024-03-14 20:55:48 +0000

    Section error log and delete logs at start

--- lemmy/test-controllers.bash
+++ lemmy/test-controllers.bash
@@ -3,33 +3,22 @@
 # Thx chatgpt for the raw version of this
 
 controllers=(
-    "comment-like" 
-    "community-add-mod" 
-    "community-ban"
-    "community-hide"
-    "user-ban-person"
-    "post-like"
-    "post-lock"
-    "post-sticky"
+    "registration-approve"
+    "site-read"
     "comment-create"
-    "comment-delete"
-    "comment-remove"
-	"community-remove"
-	"community-update"
-	"post-create"
-	"post-delete"
-	"post-remove"
-	"post-update"
-    "private-message-delete"
 )
 
 # Log file path
 log_file="log.txt"
 err_log="err.txt"
 
+rm $log_file
+rm $err_log
+
 # Function to execute commands with timeout
 execute_command() {
     local controller=$1
+    echo "$controller" >> "$err_log"
     timeout 5m cargo paralegal-flow --abort-after-analysis --target lemmy_api --verbose -- --features $controller >> "$log_file" 2>> "$err_log"
     local exit_code=$?
     if [ $exit_code -eq 124 ]; then
@@ -39,6 +28,8 @@ execute_command() {
     else
         echo "$(date) - Exit code for controller '$controller': $exit_code" >> "$log_file"
     fi
+    echo >> "$err_log"
+    echo >> "$log_file"
 }
 
 # Iterate over each command and execute with timeout
