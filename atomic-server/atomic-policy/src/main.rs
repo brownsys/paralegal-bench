d0afa870b76d4477707c525b074b8f98a05ab0f3
Justus Adam <justbldr@amazon.com>
2024-03-15 09:58:37 -0400

    Update error messages in atomic policy

--- atomic-server/atomic-policy/src/main.rs
+++ atomic-server/atomic-policy/src/main.rs
@@ -8,7 +8,7 @@ use anyhow::{bail, Result};
 use paralegal_policy::{
     assert_error, assert_warning,
     paralegal_spdg::{traverse::EdgeSelection, GlobalNode, Identifier, Node},
-    Context, Diagnostics, Marker,
+    Context, Diagnostics, GraphLocation, Marker,
 };
 use std::sync::Arc;
 
@@ -33,7 +33,6 @@ trait ContextExt {
     fn marked_nodes<'a>(&'a self, marker: Marker) -> Box<dyn Iterator<Item = GlobalNode> + 'a>;
 
     fn determines_ctrl(&self, influencer: GlobalNode, target: GlobalNode) -> bool;
-
 }
 
 impl ContextExt for Context {
@@ -58,9 +57,17 @@ policy!(check_rights, ctx {
     let commits = ctx.marked_nodes(marker!(commit));
     let mut any_sink_reached = false;
     for commit in commits {
+        let check_rights = marker!(check_rights);
         // If commit is stored
         let mut stores = ctx.influencees(commit, EdgeSelection::Both)
             .filter(|s| ctx.has_marker(marker!(sink), *s))
+            .peekable();
+
+        let mut stores = ctx
+            // .all_nodes_for_ctrl(commit.controller_id())
+            // .filter(|n| ctx.has_marker(marker!(sink), *n))
+            .marked_nodes(marker!(sink))
+            .filter(|s| ctx.flows_to(commit, *s, EdgeSelection::Both))
             .peekable();
 
         if stores.peek().is_none() {
@@ -70,24 +77,36 @@ policy!(check_rights, ctx {
 
         let new_resources = ctx.influencees(commit, EdgeSelection::Data)
             .filter(|n| ctx.has_marker(marker!(new_resource), *n))
-            .collect::<Vec<_>>();
+            .collect::<Box<[_]>>();
 
         // All checks that flow from the commit but not from a new_resource
         let valid_checks = ctx.influencees(commit, EdgeSelection::Data)
             .filter(|check|
-                ctx.has_marker(marker!(check_rights), *check)
+                ctx.has_marker(check_rights, *check)
                 && new_resources.iter().all(|r| !ctx.flows_to(*r, *check, EdgeSelection::Data))
-            )
-            .collect::<Vec<_>>();
-        assert_error!(
-            ctx,
-            !valid_checks.is_empty(),
-            format!(
-                "Found no valid checks for commit {} which flows into {}",
-                ctx.describe_node(commit),
-                ctx.describe_node(*stores.peek().unwrap())
-            )
-        );
+            ).peekable();
+
+
+        let mut valid_checks = ctx.marked_nodes(check_rights)
+            .filter(|n| ctx.flows_to(commit, *n, EdgeSelection::Data))
+            .filter(|n| ctx.any_flows(&new_resources, &[*n], EdgeSelection::Data).is_none())
+            .peekable();
+
+        if valid_checks.peek().is_none() {
+            let mut err = ctx.struct_node_error(commit, "No valid checks found for this commit");
+            for store in stores {
+                err.with_node_warning(store, "Commit reaches this store");
+            }
+
+            for check in ctx.marked_nodes(check_rights) {
+                if ctx.any_flows(&new_resources, &[check], EdgeSelection::Data).is_some() {
+                    err.with_node_note(check, "This would be a valid check, but it is influenced by `new_resource`");
+                } else {
+                    err.with_node_note(check, "This would be a valid check but it is not influenced by the commit");
+                }
+            }
+            err.emit();
+        }
 
         // BELOW IS VALID POLICY CODE BUT DOESN'T WORK BC OF PARALEGAL BUG ------
         // for store in stores {
@@ -109,6 +128,8 @@ policy!(check_rights, ctx {
 struct Arguments {
     #[clap(long)]
     buggy: bool,
+    #[clap(long)]
+    skip_compile: bool,
     #[clap(long, default_value = "..")]
     directory: std::path::PathBuf,
 }
@@ -116,18 +137,23 @@ struct Arguments {
 fn main() -> Result<()> {
     let dir = "../";
     let args: &'static _ = Box::leak(Box::new(Arguments::parse()));
-    let mut cmd = paralegal_policy::SPDGGenCommand::global();
-    cmd.external_annotations("external-annotations.toml")
-        .abort_after_analysis();
+    let graph_loc = if args.skip_compile {
+        GraphLocation::std(dir)
+    } else {
+        let mut cmd = paralegal_policy::SPDGGenCommand::global();
+        cmd.external_annotations("external-annotations.toml")
+            .abort_after_analysis();
 
-    cmd.get_command()
-        .args(["--target", "atomic_lib", "--", "--lib", "--features", "db"]);
+        cmd.get_command()
+            .args(["--target", "atomic_lib", "--", "--lib", "--features", "db"]);
 
-    if !args.buggy {
-        cmd.get_command().args(["--features", "bug-fix"]);
-    }
+        if !args.buggy {
+            cmd.get_command().args(["--features", "bug-fix"]);
+        }
+        cmd.run(dir)?
+    };
 
-    let result = cmd.run(dir)?.with_context(check_rights)?;
+    let result = graph_loc.with_context(check_rights)?;
     println!(
         "Policy {}successful with {}",
         if result.success { "" } else { "un" },
