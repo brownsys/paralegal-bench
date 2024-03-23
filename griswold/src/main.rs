e6799b5b9ed40fb72ac7f3f95c60cbed54ff503d
Justus Adam <dev@justus.science>
2024-03-23 16:03:05 -0400

    Basic documentation

--- griswold/src/main.rs
+++ griswold/src/main.rs
@@ -8,10 +8,31 @@ pub mod input;
 pub mod output;
 pub mod run;
 
+/// Run benchmarks for the 2024 SOSP Paralegal paper.
+///
+/// The run is entirely guided by a [Config], which is read from the config-path
+/// argument (defaults to "bench-config.toml").
+///
+/// Results are written to "result-path" (defaults to "results"). Each time you
+/// call this program it creates a new directory called "run-<current time in
+/// seconds>".
+///
+/// Into this directory it creates the following files:
+///
+/// - "results.csv": incrementally written statistics and results for each run.
+///   Type [output::RunStat]
+/// - "controllers.csv": incrementally written statistics about individual
+///   controllers. Type [output::ControllerStat]. Multiple such statistics are
+///   written for a single run. The "run_id" field tells you which run each row
+///   belongs to.
+/// - "sys.toml": information about the system that this experiment was run on.
+///   Type [output::SysStat]
 #[derive(clap::Parser)]
 pub struct Arguments {
+    /// Where to find the configuration file for this run
     #[clap(default_value = "bench-config.toml")]
     config_path: PathBuf,
+    /// Umbrella folder into which results should be written
     #[clap(default_value = "results")]
     result_path: PathBuf,
 }
