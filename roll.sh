b98c693b92f1d57b48a2e8b35d9f4cdf08e7cedd
Justus Adam <justbldr@amazon.com>
2024-03-26 22:27:36 +0000

    Rollin'

--- roll.sh
+++ roll.sh
@@ -4,6 +4,8 @@ LOG=$(pwd)/roll-log.txt
 echo "" > $LOG
 CODE=0
 
+FIX_LOG=$(pwd)/fix-log.txt
+
 while true
 do
     HASH=$(git -C $ATOMIC rev-parse HEAD)
@@ -13,7 +15,10 @@ do
     cargo run --bin atomic -- $ATOMIC --buggy --annotations $(pwd)/roll-forward/atomic/external-annotations.toml -- --analyze crate::commit::Commit::apply_opts --adaptive-depth >> $LOG 2>&1
     EXIT=$?
     echo "Exit" $EXIT
-    if [ $EXIT -ne $CODE ];
+    if [ $EXIT -eq 1 ];
+    then
+        echo $HASH "is broken" >> $FIX_LOG
+    elif [ $EXIT -ne $CODE ];
     then
         exit
     fi
