d9476d70705947c2ed988e53e8c96ee44f69e8b1
Justus Adam <dev@justus.science>
2024-04-15 19:00:32 -0400

    Add lemmy controllers

--- griswold/src/preparation/lemmy.rs
+++ griswold/src/preparation/lemmy.rs
@@ -7,7 +7,82 @@ use crate::{
     run::Run,
 };
 
-use super::{selection_or_all, RunBuilder};
+use super::{no_policy, selection_or_all, RunBuilder};
+
+const LEMMY_CONTROLLERS: &[&str] = &[
+    "comment-like",
+    "comment-mark-as-read",
+    "comment-save",
+    "comment-report-create",
+    "comment-report-list",
+    "comment-report-resolve",
+    "community-add-mod",
+    "community-ban",
+    "community-block",
+    "community-follow",
+    "community-hide",
+    "community-transfer",
+    "notification-list-mentions",
+    "notification-list-replies",
+    "notification-mark-all-read",
+    "notification-mark-mention-read",
+    "notification-unread-count",
+    "user-add-admin",
+    "user-ban-person",
+    "user-block",
+    "user-change-password",
+    "user-list-banned",
+    "user-login",
+    "user-report-count",
+    "user-save-settings",
+    "post-like",
+    "post-lock",
+    "post-mark-read",
+    "post-save",
+    "post-sticky",
+    "post-report-create",
+    "post-report-list",
+    "post-report-resolve",
+    "private-message-mark-read",
+    "purge-comment",
+    "purge-community",
+    "purge-person",
+    "purge-post",
+    "registration-approve",
+    "registration-list",
+    "registration-unread-counts",
+    "site-leave-admin",
+    "site-mod-log",
+    "site-resolve-object",
+    "site-search",
+    "comment-create",
+    "comment-delete",
+    "comment-list",
+    "comment-read",
+    "comment-remove",
+    "comment-update",
+    "community-create",
+    "community-delete",
+    "community-list",
+    "community-read",
+    "community-remove",
+    "community-update",
+    "post-create",
+    "post-delete",
+    "post-list",
+    "post-read",
+    "post-remove",
+    "post-update",
+    "private-message-create",
+    "private-message-delete",
+    "private-message-read",
+    "private-message-update",
+    "site-create",
+    "site-read",
+    "site-update",
+    "user-delete",
+    "user-read",
+];
 
 impl<'a> RunBuilder<'a> {
     pub(super) fn lemmy_case_study(
@@ -56,6 +131,7 @@ impl<'a> BatchConfigPreparer<'a> {
         let (policy_name, policy_fn, _) = match self.builder.experiment_config.policy_mode {
             PolicyMode::Unified => self.builder.unified_policies(),
             PolicyMode::Separate => (prop.as_ref(), Rc::new(|ctx| prop.run(ctx)) as _, vec![]),
+            PolicyMode::None => no_policy(),
         };
         let controllers = controller_features.iter().flat_map(|i| i.iter()).copied();
         let mut exp = self.builder.case_study_run(
@@ -107,7 +183,7 @@ impl<'a> BatchConfigPreparer<'a> {
                     )))
                 }
             }
-            ControllerRunMode::All => unreachable!(),
+            ControllerRunMode::All | ControllerRunMode::AllSeparate => unreachable!(),
         }
     }
 
@@ -126,55 +202,60 @@ impl<'a> BatchConfigPreparer<'a> {
     fn runs(self) -> Box<dyn Iterator<Item = Run<'a>> + 'a> {
         let (initial_extra_feature, changed_extra_feature) = self.extra_features();
         let change = &self.batch_config.change;
-        if matches!(
-            self.builder.experiment_config.controller_run_mode,
-            ControllerRunMode::All
-        ) {
-            Box::new(
-                [
-                    self.case_study_run(
-                        slice::from_ref(&slice::from_ref(&"all-controllers")),
-                        self.batch_config.expect_failure,
-                        initial_extra_feature,
-                    ),
-                    self.case_study_run(
-                        slice::from_ref(&slice::from_ref(&"all-controllers")),
-                        !self.batch_config.expect_failure,
-                        changed_extra_feature,
-                    ),
-                ]
-                .into_iter(),
-            ) as Box<_>
-        } else {
-            let iter = self
-                .mk_batch_exps(
+        let run_pair = move |ctrl| {
+            [
+                self.case_study_run(
+                    slice::from_ref(&slice::from_ref(&ctrl)),
                     self.batch_config.expect_failure,
-                    self.batch_config.baseline_controllers,
                     initial_extra_feature,
-                )
-                .chain(change.iter().flat_map(move |change| {
-                    change.affected_controllers.iter().flat_map(move |c| {
-                        self.mk_batch_exps(
-                            self.batch_config.expect_failure,
-                            slice::from_ref(c),
-                            initial_extra_feature,
-                        )
-                    })
-                }))
-                .chain(change.iter().flat_map(move |change| {
-                    let fixed = change
-                        .affected_controllers
-                        .as_ref()
-                        .map_or(self.batch_config.baseline_controllers, |ctrl| {
-                            slice::from_ref(ctrl)
-                        });
-                    self.mk_batch_exps(
-                        !self.batch_config.expect_failure,
-                        fixed,
-                        changed_extra_feature,
+                ),
+                self.case_study_run(
+                    slice::from_ref(&slice::from_ref(&ctrl)),
+                    !self.batch_config.expect_failure,
+                    changed_extra_feature,
+                ),
+            ]
+            .into_iter()
+        };
+        match self.builder.experiment_config.controller_run_mode {
+            ControllerRunMode::All => Box::new(run_pair("all_controllers")) as Box<_>,
+            ControllerRunMode::AllSeparate => Box::new(
+                LEMMY_CONTROLLERS
+                    .iter()
+                    .copied()
+                    .flat_map(move |c| run_pair(&c)),
+            ),
+            _ => {
+                let iter = self
+                    .mk_batch_exps(
+                        self.batch_config.expect_failure,
+                        self.batch_config.baseline_controllers,
+                        initial_extra_feature,
                     )
-                }));
-            Box::new(iter)
+                    .chain(change.iter().flat_map(move |change| {
+                        change.affected_controllers.iter().flat_map(move |c| {
+                            self.mk_batch_exps(
+                                self.batch_config.expect_failure,
+                                slice::from_ref(c),
+                                initial_extra_feature,
+                            )
+                        })
+                    }))
+                    .chain(change.iter().flat_map(move |change| {
+                        let fixed = change
+                            .affected_controllers
+                            .as_ref()
+                            .map_or(self.batch_config.baseline_controllers, |ctrl| {
+                                slice::from_ref(ctrl)
+                            });
+                        self.mk_batch_exps(
+                            !self.batch_config.expect_failure,
+                            fixed,
+                            changed_extra_feature,
+                        )
+                    }));
+                Box::new(iter)
+            }
         }
     }
 }
