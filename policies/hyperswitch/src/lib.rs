80da693083192c57a671ff9ed679fcf44d57fa87
Justus Adam <dev@justus.science>
2025-04-20 13:10:52 -0400

    Further hyperswitch optimizations

--- policies/hyperswitch/src/lib.rs
+++ policies/hyperswitch/src/lib.rs
@@ -1,7 +1,9 @@
 use anyhow::Result;
 use paralegal_policy::{
-    assert_error, assert_warning, paralegal_spdg::Identifier, Context, Diagnostics, EdgeSelection,
-    Marker, NodeExt, NodeQueries, RootContext,
+    assert_error, assert_warning,
+    paralegal_spdg::{DisplayPath, Identifier, NodeCluster},
+    Context, Diagnostics, EdgeSelection, IntoIterGlobalNodes, Marker, NodeExt, NodeQueries,
+    RootContext,
 };
 use serde::{Deserialize, Serialize};
 use std::{collections::HashSet, sync::Arc};
@@ -101,21 +103,68 @@ policy!(card_storage, ctx {
             .nodes_marked_any_way(m_credit_card)
             .filter(|n| n.controller_id() == ctx.id())
             .peekable();
-        let decision_sources = ctx
-            .nodes_marked_any_way(m_future_usage)
-            .filter(|n| n.controller_id() == ctx.id())
-            .collect::<Vec<_>>();
+        if srcs.peek().is_none() {
+            continue;
+        }
+        let Some(decision_sources) = NodeCluster::try_from_iter(
+            ctx.nodes_marked_any_way(m_future_usage)
+                .filter(|n| n.controller_id() == ctx.id())
+            ) else {
+            let mut msg = ctx.struct_error("No future usage decision found");
+            for src in srcs {
+                msg.with_node_note(src, "Credit card source");
+            }
+            msg.emit();
+            continue;
+        };
         any_source_found = any_source_found || srcs.peek().is_some();
-        for src in srcs {
-            for sink in src.influencees(&ctx, EdgeSelection::Data).into_iter().filter(|n| n.has_marker(&ctx, m_store)) {
-                any_sink_reached = true;
+        // for src in srcs {
+        //     for sink in src.influencees(&ctx, EdgeSelection::Data).into_iter().filter(|n| n.has_marker(&ctx, m_store)) {
+        //         any_sink_reached = true;
 
-                let decision_reaches = decision_sources.iter().cloned().any(|decision_source|
-                    decision_source.has_ctrl_influence(sink, &ctx)
-                );
-                assert_error!(ctx, decision_reaches);
+        //         if let Some(_) = decision_sources.has_ctrl_influence_all(sink, &ctx) {
+        //             let mut msg = ctx.struct_error("Unprotected credit card storage");
+        //             msg.with_node_note(src, "Credit card source");
+        //             msg.with_node_note(sink, "Credit card storage");
+        //             msg.emit();
+        //         }
+        //     }
+        // }
+
+        // Optimization 1: Ask control influence on all sinks for one credit
+        // card value at the same time.
+        for src in srcs {
+            let Some(sinks) = NodeCluster::try_from_iter(src.influencees(&ctx, EdgeSelection::Data).into_iter().filter(|n| n.has_marker(&ctx, m_store))) else {
+                continue;
+            };
+            any_sink_reached = true;
+
+            if let Some(unreached) = decision_sources.has_ctrl_influence_all(&sinks, &ctx) {
+                let mut msg = ctx.struct_error("Unprotected credit card storage");
+                msg.with_node_note(src, "Credit card source");
+                for sink in unreached.iter_global_nodes() {
+                    msg.with_node_note(sink, "Credit card storage");
+                }
+                msg.emit();
             }
         }
+
+        // Optimization 2: Ask control influence on all sinks for all credit
+        // card values at the same time.
+        // if let Some(sinks) = NodeCluster::try_from_iter(srcs.
+        //     flat_map(|src| src.influencees(&ctx, EdgeSelection::Data))
+        //     .filter(|n| n.has_marker(&ctx, m_store)))
+        // {
+        //     any_sink_reached = true;
+        //     if let Some(unreached) = decision_sources.has_ctrl_influence_all(&sinks, &ctx) {
+        //         let mut msg = ctx.struct_error("Unprotected credit card storage");
+        //         for sink in unreached.iter_global_nodes() {
+        //             msg.with_node_note(sink, "Credit card storage");
+        //         }
+        //         msg.emit();
+        //     }
+        // }
+
     }
     assert_warning!(ctx, any_source_found, "VACUITY: No sensitive sources found");
     assert_warning!(ctx, any_sink_reached, "VACUITY: No sensitive sinks ever reached.");
